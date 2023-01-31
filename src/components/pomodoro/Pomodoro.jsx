3902487e62eb577c48fc9e15388fc3a94c92082d
Martin Reyes <martinreyesrf@gmail.com>
2023-01-31 13:17:49 -0600

    sound click agregado y react-howler para manejo de sonido

--- src/components/pomodoro/Pomodoro.jsx
+++ src/components/pomodoro/Pomodoro.jsx
@@ -1,11 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react'
-
+import ReactHowler from 'react-howler'
+import soundClick from '../../assets/sounds/RUST_HitSoundEffect.mp3'
 export const Pomodoro = ({focusTime = 10, freetime = 5}) => {
 
   const [pomodoroType, setPomodoroType] = useState({
     focusTime,
     freetime
   })
+
+  //click sound
+  const [isClick, setIsClick] = useState(false)
+  //
 
   const [time, setTime] = useState(focusTime * 60);
   const [seconds, setSeconds] = useState(focusTime * 60);
@@ -16,15 +21,17 @@ export const Pomodoro = ({focusTime = 10, freetime = 5}) => {
   const [isFocus, setIsFocus] = useState(true)
 
   const handleStart = useCallback(() => {
-    setIsRunning(true)
+    setIsRunning(true);
+    setIsClick(true)
   },[],);
   
   const handleStop = useCallback(() => {
-    setIsRunning(false)
+    setIsRunning(false);
+    setIsClick(true);
   },[],);
 
   const handlerReset = useCallback(() => {
-
+    setIsClick(true);
     const {focusTime,freetime} = pomodoroType;
     if(isFocus){
       setTime(focusTime * 60)
@@ -126,37 +133,45 @@ export const Pomodoro = ({focusTime = 10, freetime = 5}) => {
 
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center gap-y-4'>
-        <h1 className='text-5xl font-sans'>
-          {isFocus ? 'Focus time' : 'Free time'}
-        </h1>
-        <h1 className='font-mono text-7xl'>
-          {`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
-        </h1>
+      
+      <ReactHowler 
+        src={soundClick}
+        playing={isClick}
+        loop={false}
+        onEnd={() => setIsClick(false)}
+      />
 
-        <div className='flex'>
-          <button 
-            className='btn_custum'
-            style={isRunning ? {display:'none'} : {display: 'block'}} 
-            onClick={handleStart} 
-            disabled={isRunning}>
-            Start
-          </button>
-          <button 
-            className='btn_custum'
-            style={!isRunning ? {display:'none'} : {display: 'block'}} 
-            onClick={handleStop} 
-            disabled={!isRunning}>
-            Stop
-          </button>
-          <button
-            className='btn_custum'
-            onClick={handlerReset}>
-            Reset
-          </button>
-        </div>
-        <div className='border-4 border-indigo-600 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-96 w-96 absolute -z-20 rounded-full'></div>
-        <div className='bg-white h-80 w-80 absolute -z-10 rounded-full'></div>
-        {!isRunning && time == 0? <p>Termino</p> : null}
+      <h1 className='text-5xl font-sans'>
+        {isFocus ? 'Focus time' : 'Free time'}
+      </h1>
+      <h1 className='font-mono text-7xl'>
+        {`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
+      </h1>
+
+      <div className='flex'>
+        <button 
+          className='btn_custum'
+          style={isRunning ? {display:'none'} : {display: 'block'}} 
+          onClick={handleStart} 
+          disabled={isRunning}>
+          Start
+        </button>
+        <button 
+          className='btn_custum'
+          style={!isRunning ? {display:'none'} : {display: 'block'}} 
+          onClick={handleStop} 
+          disabled={!isRunning}>
+          Stop
+        </button>
+        <button
+          className='btn_custum'
+          onClick={handlerReset}>
+          Reset
+        </button>
+      </div>
+      <div className='border-4 border-indigo-600 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-96 w-96 absolute -z-20 rounded-full'></div>
+      <div className='bg-white h-80 w-80 absolute -z-10 rounded-full'></div>
+      {!isRunning && time == 0? <p>Termino</p> : null}
     </div>
   )
 }
