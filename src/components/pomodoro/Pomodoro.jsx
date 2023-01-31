6c5d60bf16dd6e174b7c0b9d9ca2429e13628fd4
Martin Reyes <martinreyesrf@gmail.com>
2023-01-30 18:16:15 -0600

    cambio de pomodoro(focus a free)

--- src/components/pomodoro/Pomodoro.jsx
+++ src/components/pomodoro/Pomodoro.jsx
@@ -1,26 +1,52 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
-export const Pomodoro = ({focuesTime = 5}) => {
+export const Pomodoro = ({focusTime = 10, freetime = 5}) => {
 
-  const [time, setTime] = useState(focuesTime * 60)
-  const [seconds, setSeconds] = useState(focuesTime * 60);
-  const [minutes, setMinutes] = useState(focuesTime);
-  const [hours, setHours] = useState(focuesTime / 60);
+  const [pomodoroType, setPomodoroType] = useState({
+    focusTime,
+    freetime
+  })
+
+  const [time, setTime] = useState(focusTime * 60);
+  const [seconds, setSeconds] = useState(focusTime * 60);
+  const [minutes, setMinutes] = useState(focusTime);
+  const [hours, setHours] = useState(focusTime / 60);
   const [isRunning, setIsRunning] = useState(false);
+  // Cambio de pomodoro
+  const [isFocus, setIsFocus] = useState(true)
 
   const handleStart = useCallback(() => {
     setIsRunning(true)
   },[],);
-  
   
   const handleStop = useCallback(() => {
     setIsRunning(false)
   },[],);
 
   const handlerReset = useCallback(() => {
-    setSeconds(focuesTime * 60)
-    setMinutes(focuesTime);
-    setHours(focuesTime / 60)
+
+    const {focusTime,freetime} = pomodoroType;
+    if(isFocus){
+      setTime(focusTime * 60)
+      setSeconds(focusTime * 60)
+      setMinutes(focusTime);
+      setHours(focusTime / 60)
+
+      shapeTime();
+      setIsRunning(false)
+    }else{
+      setTime(freetime * 60)
+      setSeconds(freetime * 60)
+      setMinutes(freetime);
+      setHours(freetime / 60)
+
+      shapeTime();
+      setIsRunning(false)
+    }
+  },[isFocus],);
+
+  //formatear el tiempo
+  const shapeTime = () => {
     if(seconds >= 59){
       setSeconds(0)
     }
@@ -30,9 +56,36 @@ export const Pomodoro = ({focuesTime = 5}) => {
     if(hours < 1){
       setHours(0)
     }
-    setIsRunning(false)
-  },[],);
+  }
+
+  // Cambio de pomodoro
   
+  const pomodoroFree = () => {
+    const {freetime} = pomodoroType
+    setTime(freetime * 60);
+    setSeconds(freetime * 60);
+    setMinutes(freetime);
+    setHours(freetime / 60);
+    shapeTime();
+
+    setIsFocus(false);
+    // setIsRunning(false)
+  }
+
+  const pomodoroFocus = () => {
+    // setIsRunning(false)
+
+    const {focusTime} = pomodoroType;
+
+    setTime(focusTime * 60);
+    setSeconds(focusTime * 60);
+    setMinutes(focusTime);
+    setHours(focusTime / 60);
+    shapeTime();
+
+    setIsFocus(true);
+  };
+
   const updateTimie = () => {
     setTime(time => time - 1);
     setSeconds(preSeconds => preSeconds - 1)
@@ -56,9 +109,11 @@ export const Pomodoro = ({focuesTime = 5}) => {
     if(isRunning){
       intervalId = setInterval(() => {
         updateTimie();
-      },10)
+      },1000)
     }else if(!isRunning && time == 0){
       clearInterval(intervalId);
+      //implementacion de cambio de pomodoro
+      isFocus ? pomodoroFree() : pomodoroFocus();
     };
     return () => {
       clearInterval(intervalId)
@@ -66,20 +121,12 @@ export const Pomodoro = ({focuesTime = 5}) => {
   }, [time, isRunning]);
 
   useEffect(() => {
-    if(seconds >= 59){
-      setSeconds(0)
-    }
-    if(minutes >= 59){
-      setMinutes(0)
-    }
-    if(hours < 1){
-      setHours(0)
-    }
-    
-  },[]);
+    shapeTime();
+  },[isFocus]);
 
   return (
     <div>
+        <h1>{isFocus ? 'Focus time' : 'Free time'}</h1>
         <h1>Timer: {`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</h1>
         <button onClick={handleStart} disabled={isRunning}>Start</button>
         <button onClick={handleStop} disabled={!isRunning}>Stop</button>
