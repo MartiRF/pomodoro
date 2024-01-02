08c1c16b7f7eb5383950dbb82f8f411f5e91d660
MartiRF <martinreyesrf@gmail.com>
2024-01-02 10:06:12 -0600

    Config.jsx: state de tiempo agregado

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,28 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Modal } from "./Modal"
 
 export const Config = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const [time, setTime] = useState({focus:5,minutos:10})
+  const [time, setTime] = useState({
+    focus:10,
+    free:5,
+  })
 
   const onAplicar = () => {
     setIsOpen(false);
   }
+//
 
+  const handlerSumit = (e) => {
+    e.preventDefault();
+  }
+  const handlerChange = ({target}) => {
+    let {name,value} = target;
+    if(value > 60 ){
+      value = 59;
+    }
+    const newTime = {
+      ...time,
+      [name]:value
+    }
+    setTime(newTime)
+  }
+  //
   return (
     <>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(!isOpen)}>
         <div className="flex flex-col m-2 justify-center items-center">
           <h1 className="text-3xl">Configuracion</h1>
-          <form className="mt-2">
+          <form className="mt-2" onSubmit={handlerSumit}>
             <div className="flex flex-col items-center justify-start w-full min-w-[8rem]">
               <div className="flex justify-between w-full pt-1">
                 <label>Focus:</label>
                 <input
-                  className="border-solid border-2 border-yellow-400 rounded-lg"
-                  id="minutosFocus"
+                  id="focus"
+                  name="focus"
                   type="number"
-                  name="minutosFocus"
+                  value={time.focus}
+                  onChange={handlerChange}
+                  className="border-solid border-2 border-yellow-400 rounded-lg"
                   step="1"
                   min="1"
                   max="59"
@@ -31,10 +52,12 @@ export const Config = () => {
               <div className="flex justify-between w-full pt-1">
                 <label >Free:</label>
                 <input
-                  className="border-solid border-2 border-yellow-400 rounded-lg"
                   id="minutosFree"
+                  name="free"
                   type="number"
-                  name="minutosFree"
+                  value={time.free}
+                  onChange={handlerChange}
+                  className="border-solid border-2 border-yellow-400 rounded-lg"
                   step="1"
                   min="1"
                   max="59"
